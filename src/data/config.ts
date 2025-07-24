fa3307017089b4ad6c5c6e4544c21b90e055238c
SG-TRINITY <shrishty@ualberta.ca>
2025-07-24 10:38:06 -0600

    fix: uncomment twitter handle in config

--- src/data/config.ts
+++ src/data/config.ts
@@ -3,7 +3,7 @@ const config = {
   url: process.env.NEXT_PUBLIC_PORTFOLIO_URL!,
   defaultDescription: 'I’m Shrishty G!',
   googleAnalyticsID: 'G-PE8QW0ZGTZ',
-  //twitter: '@smakosh',
+  twitter: '@smakosh',
 };
 
 export default config;
