4da45cd024cc6387066f47f5b6936c8833ebd382
SG-TRINITY <shrishty@ualberta.ca>
2025-07-24 14:37:50 -0600

    refactor: update Skills component description to enhance clarity and focus

--- src/components/modules/Skills/index.tsx
+++ src/components/modules/Skills/index.tsx
@@ -15,7 +15,8 @@ const Skills = () => (
       <div className="flex-none lg:flex-1 pl-0 lg:pl-8">
         <h2 className="mb-8 text-3xl font-bold text-brand-primary dark:text-white typography">More about me</h2>
         <p className="mb-[2.5rem] font-normal text-xl md:text-[26px] text-brand-secondary dark:text-slate-200 leading-[34px] typography">
-          I’m Shrishty, a gal who just wants to code and create amazing things. I am a Computer Science student with a passion for Frontend development and design. I love building beautiful and user-friendly interfaces that make the web a better place.
+          I’m Shrishty, a gal who just wants to code and create amazing things. I am a Computer Science student with a passion for Frontend development. I love building user-friendly interfaces and exploring the latest technologies. When I’m not coding, you can find me reading books or painting my nails.
+        </p>
         <Link href="#contact" className="button button-primary">
           Hire me
         </Link>
