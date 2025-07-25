90dfe6d133dfd2c3cd030470a1ede9eb7268ecc2
SG-TRINITY <shrishty@ualberta.ca>
2025-07-25 15:37:28 -0600

    refactor: update footer text color and social media icon sizes for consistency

--- src/components/ui/theme/Footer/index.tsx
+++ src/components/ui/theme/Footer/index.tsx
@@ -5,15 +5,15 @@ const Footer = () => (
   <div className="pt-56 pb-16 bg-[url('/assets/illustrations/footer.svg')] bg-cover bg-top bg-no-repeat">
     <div className="container flex items-center md:items-end text-center md:text-left justify-between flex-col md:flex-row">
       <div className="mb-8 md:mb-0">
-        <h2 className="text-brand-primary min-[2170px]:dark:text-white font-bold text-2xl mb-4">Shrishty G</h2>
-        <span className="text-brand-primary min-[2170px]:dark:text-white">
+        <h2 className="text-white min-[2170px]:dark:text-white font-bold text-2xl mb-4">Shrishty G</h2>
+        <span className="text-white min-[2170px]:dark:text-white">
           © All rights are reserved | {new Date().getFullYear()} | Original template made {' '}
           by{' '}
           <a
             href="https://smakosh.com/?ref=portfolio-dev"
             rel="noopener noreferrer"
             target="_blank"
-            className="text-brand-primary min-[2170px]:dark:text-white"
+            className="text-white min-[2170px]:dark:text-white"
           >
             Smakosh
           </a>
@@ -27,9 +27,9 @@ const Footer = () => (
             target="_blank"
             rel="noopener noreferrer"
             aria-label={`follow me on ${name}`}
-            className="mr-2 last:mr-0"
+            className="mr-6 last:mr-10"
           >
-            <Image src={icon} alt={name} className="w-6 h-6" width={24} height={24} />
+            <Image src={icon} alt={name} className="w-9 h-9" width={36} height={36} />
           </a>
         ))}
       </div>
